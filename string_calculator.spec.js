eef469c991c0126bf43b3fe9fdd189f529295ff0
harsh.harsola <harsh.harsola@quillbot.com>
2024-02-17 12:09:59 +0530

    added extra test to check for multiple numbers

--- string_calculator.spec.js
+++ string_calculator.spec.js
@@ -8,4 +8,8 @@ describe("test suite for string calculator", () => {
   test("sum of 5,6", () => {
     expect(string_calculator("5,6")).toBe(11);
   });
+
+  test("sum of 5,6,7,8,9", () => {
+    expect(string_calculator("5,6,7,8,9")).toBe(35);
+  });
 });
